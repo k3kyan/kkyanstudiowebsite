4c25e03ce29e3c1f77b0fdf5c43fa82f07f56f07
Lolozo <samsimonzhang@gmail.com>
2023-07-22 06:51:27 -0700

    made input forms for commission page

--- README.md
+++
@@ -1,5 +0,0 @@
-
-# kkyanstudiowebsite
-
-
-sdlkfj
---
+++ reactver/src/CommissionsPreform.js
@@ -0,0 +1,11 @@
+
+import './CommissionsPreform.css'
+function CommissionsPreform(){
+    return (
+        <div>
+        <h1 className="topic">Insert a custom commission request</h1>
+        <img className="img" src="./commissionsimg.png" alt="Failed to load."/></div>
+    );
+}
+
+export default CommissionsPreform;
